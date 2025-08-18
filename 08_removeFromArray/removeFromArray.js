358a32ab6460220d74c88ee70bd2d27da3c18a77
Danny <danielvalledubon@gmail.com>
2025-08-18 16:25:20 -0700

    implemented removeFromArray function
    The function takes an array and an indefinite amount of numbers as its parameter.
    It then filters the array to only include numbers that aren't present in the parameter.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    return arr.filter(val => !args.includes(val)); /* filter the array to only include
+    numbers where the inputed values are removed*/
 };
 
 // Do not edit below this line
