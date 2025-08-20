2c8294abf7c85591c8f7f7de6f48ff34f59874b0
Danny <danielvalledubon@gmail.com>
2025-08-20 16:06:39 -0700

    completion of sumAll function
    fully implemented the function as well as all edge cases.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    let sum = 0;
 
+    if (firstNum < 0 || secondNum < 0) {
+        return "ERROR";
+    }
+
+    if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) {
+        return "ERROR";
+    }
+
+    if (secondNum > firstNum) {
+        for (let i = firstNum; i <= secondNum; i++) {
+            sum += i;
+        }
+    } else if (firstNum > secondNum) {
+        for (let i = firstNum; i >= secondNum; i--) {
+            sum += i;
+        }
+    }
+    
+
+    return sum;
 };
 
 // Do not edit below this line
