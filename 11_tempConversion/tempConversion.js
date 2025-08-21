ecb7e3647f8fbe424234299a70be27614046a4f4
Danny <danielvalledubon@gmail.com>
2025-08-21 10:49:10 -0700

    implemented both conversion functions
    convertToCelsius takes a fahrenheit number and converts it to celsius and
    rounds it to the first decimal point. convertToFahrenheit does the same but
    converts top fahrenheit.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  const formula = (num - 32) * 5/9;
+  return Math.round(formula * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  const formula = num * 9/5 + 32;
+  return Math.round(formula * 10)/10;
 };
 
 // Do not edit below this line
