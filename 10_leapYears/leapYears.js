798720e2c070cc5c17762952a66b525e438422cc
Danny <danielvalledubon@gmail.com>
2025-08-21 10:23:40 -0700

    implemented the leapYears function
    defined constants for each possible leap year condition.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divisibleByFour = year % 4 === 0;
+    const divisibleByHundred = year % 100 === 0;
+    const divisibleByFourHundred = year % 400 === 0;
+
+    if (divisibleByFour && !divisibleByHundred || divisibleByFourHundred) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
